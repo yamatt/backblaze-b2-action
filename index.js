c7ae5438333617f3ef0579de452581ae36156318
Matt Copperwaite <matt@copperwaite.net>
2021-07-22 15:29:15 +0100

    Updated to upload file to B2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const core = require('@actions/core');
 const childProcess = require('child_process');
 const crypto = require('crypto');
 const fs = require('fs');
@@ -11,20 +12,20 @@ const {path, url, ex} = {
   Windows_NT: {path: 'C:\\Windows\\system32\\b2.exe', url: 'https://f000.backblazeb2.com/file/backblazefiles/b2/cli/windows/b2.exe'},
 }[os.type()];
 
-const outPath = ex ? `/tmp/${crypto.randomBytes(16).toString('hex')}` : path;
+const exec_path = ex ? `/tmp/${crypto.randomBytes(16).toString('hex')}` : path;
 
 https.get(url, res => {
   stream.pipeline(
     res,
-    fs.createWriteStream(outPath),
+    fs.createWriteStream(exec_path),
     err => {
       if (err) {
         console.error(err);
       } else if (ex) {
         fs.chmodSync(outPath, 0o555);
-        childProcess.execFileSync('sudo', ['mv', outPath, path]);
+        childProcess.execFileSync(exec_path, ["authorize-account", core.getInput('key_id'), core.getInput('application_key')]);
+        childProcess.execFileSync(exec_path, [core.getInput('bucket_name'), core.getInput('file_path'), core.getInput('remote_path')]);
       }
     }
   );
 });
-
